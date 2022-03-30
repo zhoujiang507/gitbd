4f71a9f586489e73013ae2f4ef1b6ca60b0a2184
zhoujiang507 <102548105+zhoujiang507@users.noreply.github.com>
2022-03-31 00:49:52 +0800

    初次上传

--- java1/src/main/java/aaa.java
+++ java1/src/main/java/aaa.java
@@ -3,6 +3,11 @@
 //创建一个文档
 public class aaa {
     public static void main(String[] args) {
+        //打印输出常量
         System.out.println("aaa");
+    int a = 10;
+        System.out.println(a);
+        long l = 4000000000000L;
+        System.out.println(l);
     }
 }
---
+++ java1/src/main/java/dametest.java
@@ -0,0 +1,10 @@
+public class dametest {
+    public static void main(String[] args) {
+        int a;
+        {
+          int  b=1;
+        }
+        a=1;
+        System.out.println(a);
+    }
+}
